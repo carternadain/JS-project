de29b05ac24b3ba88dcfcb1c0efb7928423db20d
carternadain <cnadain@gmail.com>
2025-02-20 21:36:27 -0700

    add if else logic

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-
-
-console.log("hello world")
-
-
-let firstCard = 5
+let firstCard = 8
 let secondCard = 10
-
 
 let sum = firstCard + secondCard
 
-console.log(sum)
+
+if (sum <= 20){
+    console.log("do you want to draw again?")
+} else if (sum === 21){
+    console.log("you've got blackjack!")
+
+} else {
+    console.log("you lost!")
+}
