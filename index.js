031a450c4a1a81a5351d1c5bd5a7678fc2fed994
carternadain <cnadain@gmail.com>
2025-02-21 17:39:45 -0700

    add booleans values

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
-let firstCard = 8
-let secondCard = 10
+let firstCard = 10
+let secondCard = 11
 
 let sum = firstCard + secondCard
-
+let hasBlackJack = false
+let isAlive = true
 
 if (sum <= 20){
     console.log("do you want to draw again?")
+   
 } else if (sum === 21){
     console.log("you've got blackjack!")
-
+    hasBlackJack = true
 } else {
     console.log("you lost!")
+    isAlive = false
 }
+
+console.log(isAlive)
