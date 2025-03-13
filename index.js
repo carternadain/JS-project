101fe58248efa086cc57ae5e15107155359bd89e
carternadain <cnadain@gmail.com>
2025-03-12 18:11:47 -0700

    testing or statement and & operators

--- index.js
+++ index.js
@@ -66,5 +66,29 @@ function newCard() {
     array.push(cardKing)
 }
 
+let hasSolvedChallenge = false
+let hasHintsLeft = false
 
-   
+// Create an if statement that checks that both variables are false.
+// If so, run the showSolution() function
+
+if (hasSolvedChallenge === false && hasHintsLeft === false){
+    showSolution()
+}
+
+function showSolution() {
+    console.log("Showing the solution....")
+}
+
+let likesDocumentaries = true
+let likesStartups = false
+// Create two boolean variables, likesDocumentaries and likesStartups
+// Use an OR statement (||) to call recommendMovie() if either of those variables are true
+if (likesDocumentaries === true || likesStartups === true){
+    recommendMovie()
+}
+
+
+function recommendMovie() {
+    console.log("Hey, check out this new film we think you will like!")
+}
