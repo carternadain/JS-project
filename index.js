8c4dd16d113cde1253ad58318811e7851fcdca47
carternadain <cnadain@gmail.com>
2025-03-22 16:57:05 -0700

    practice my for loop to iterate through an array

--- index.js
+++ index.js
@@ -177,3 +177,9 @@ if (age < 6){
 } else {
     console.log("senior citizen discount")
 }
+
+let largeCountries = ["China","India","USA","Indonesia","Pakistan"]
+
+for(i = 0; i < largeCountries.length; i++){
+    console.log(largeCountries[i])
+ }
