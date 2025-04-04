37476c6156e68d85076d383936722946ece03827
carternadain <cnadain@gmail.com>
2025-04-03 23:16:26 -0700

    practice temp literal strings

--- practice2.js
+++ practice2.js
@@ -3,3 +3,16 @@ let boxBtn = document.getElementById("box")
 boxBtn.addEventListener("click", function() {
     console.log("i want to opent he box when its clicked")
 })
+
+
+// template strings/literals
+
+const recipient = "James"
+const sender = "Per Harald Borgen"
+
+// Break the email string into multiple lines
+const email = `Hey ${recipient}! 
+How is it going? 
+Cheers ${sender}`
+
+console.log(email)
