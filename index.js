be6795a1d9402e6799afb2b10ee27a49f7967102
carternadain <cnadain@gmail.com>
2025-04-06 23:18:53 -0700

    update with comments

--- index.js
+++ index.js
@@ -3,14 +3,15 @@ const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 
-
+// listner for when user clicks button , then fires the function of the empty array myleads
+// and adds the input value the user puts 
 inputBtn.addEventListener("click", function(){
     myLeads.push(inputEl.value)
     inputEl.value = ""
     renderLeads()
 })
 
-
+// function to renderleads , loop to iterate through the array
 function renderLeads () {
     let listItems = ""
     for (let i = 0; i < myLeads.length; i++) {
@@ -25,4 +26,5 @@ function renderLeads () {
     
     ulEl.innerHTML = listItems
 }
+
 
