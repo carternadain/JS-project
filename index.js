b37b5c2fed183bef11fa8c6170e12a3c812f639f
carternadain <cnadain@gmail.com>
2025-04-07 18:17:38 -0700

    test json prop stringify and typo of prop

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
-let myLeads = []
+let myLeads = `["www.hello.com"]`
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 
+myLeads =JSON.parse(myLeads)
+
+myLeads.push("www.hello.com")
+myLeads = JSON.stringify(myLeads)
+console.log(typeof myLeads)
 // listner for when user clicks button , then fires the function of the empty array myleads
 // and adds the input value the user puts 
 inputBtn.addEventListener("click", function(){
