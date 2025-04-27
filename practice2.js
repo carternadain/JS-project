4a3f7341249ea24ab90efb4c1ee55468b33f3fa8
carternadain <cnadain@gmail.com>
2025-04-27 11:59:40 -0700

    testing with to fixed method

--- practice2.js
+++ practice2.js
@@ -149,26 +149,44 @@
 
 
 
-// id = container 
+
 // get the id transfer to inner html loop through array with for loop
 // Create a function that renders the three team images
 // Use a for loop, template strings (``), plus equals (+=)
 // .innerHTML to solve the challenge.
 
 
-const imgs = [
-    "images/hip1.jpg",
-    "images/hip2.jpg",
-    "images/hip3.jpg"
-]
+// const imgs = [
+//     "images/hip1.jpg",
+//     "images/hip2.jpg",
+//     "images/hip3.jpg"
+// ]
+
+// const container = document.getElementById("container")
+
+
+// function renderImages() {
+//     let imgsDOM = ""
+//     for (let i = 0; i < imgs.length; i++) {
+//         imgsDOM += `<img alt="Employee in the company" class="team-img" src="${imgs[i]}">`
+//     }
+//     container.innerHTML = imgsDOM
+// }
+
+// renderImages()
+
+// imgRender()
+
+// Challenge:
+// Round the price in the button down to two decimal places.
+// Don't know which method to use? Google it!
 
 
 
-function imgRender() {
-    
-    for( let i = 0; i < imgs.length; i++ )
-    console.log(imgs[i])
-    // let image = getElementById("container")
-}
+const totalPrice = 420.69235632455
 
-imgRender()
+const btn = document.getElementById("purchase-btn")
+btn.textContent = `Buy €${(Math.floor(totalPrice * 100) / 100).toFixed(2)}`
+
+
+
