76d5c4a8554334e5b23ee2a4ecfe7d7f1d4832af
carternadain <cnadain@gmail.com>
2025-05-01 20:32:41 -0700

    finish challenge

--- lightDark/script.js
+++ lightDark/script.js
@@ -3,4 +3,6 @@ let toggleBtn = document.getElementById("toggle-btn")
 
 toggleBtn.addEventListener("click", function(){
     console.log("it was clicked")
+    // toggleBtn = document.body.classList.toggle("dark-mode")
+    document.body.classList.toggle("dark-mode")
 })
