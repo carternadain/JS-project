78222804562fa827e0225b6120332ac2fcd2d007
carternadain <cnadain@gmail.com>
2025-05-06 22:10:55 -0700

    testing if else stating in game

--- numberGuess/app.js
+++ numberGuess/app.js
@@ -1,5 +1,20 @@
 let guessButton = document.getElementById("guessButton")
 let resetButton = document.getElementById("resetButton")
 let guessCount = document.getElementById("guessCount")
+let answer = document.getElementById("guessInput")
+
+guessButton.addEventListener("click", function(){
+    let randomNumber = Math.floor(Math.random() * 10 + 1)
+    if (randomNumber == answer) {
+        console.log("guessed right!")
+    }
+        else {
+            console.log("wrong")
+        }
+    console.log("it worked")
+})
 
 
+resetButton.addEventListener("click", function(){
+    console.log("it worked")
+})
