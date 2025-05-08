83a3dfeb75cd3e8a0f0c141121843995734e4e89
carternadain <cnadain@gmail.com>
2025-05-07 21:05:30 -0700

    finish guessing game

--- numberGuess/app.js
+++ numberGuess/app.js
@@ -2,19 +2,34 @@ let guessButton = document.getElementById("guessButton")
 let resetButton = document.getElementById("resetButton")
 let guessCount = document.getElementById("guessCount")
 let answer = document.getElementById("guessInput")
+let randomNumber = Math.floor(Math.random() * 10 + 1)
+let feedback = document.getElementById("feedback")
+
+
+let count = 0
+
 
 guessButton.addEventListener("click", function(){
-    let randomNumber = Math.floor(Math.random() * 10 + 1)
-    if (randomNumber == answer) {
-        console.log("guessed right!")
-    }
-        else {
-            console.log("wrong")
-        }
-    console.log("it worked")
+    count++
+    guessCount.textContent = "Guesses: " + count
+
+    let userGuess = parseInt(answer.value)
+
+    if (userGuess === randomNumber) {
+      feedback.textContent = "🎉 Guessed right!"
+    } else if (userGuess < randomNumber) {
+      feedback.textContent = "📉 Too low!"
+    } else {
+      feedback.textContent = "📈 Too high!"
+    }    
 })
 
 
 resetButton.addEventListener("click", function(){
-    console.log("it worked")
+    count = 0
+    guessCount.textContent = "Guesses: 0"
+    answer.value = ""
+    feedback.textContent = ""
+    randomNumber = Math.floor(Math.random() * 100 + 1)
+    
 })
