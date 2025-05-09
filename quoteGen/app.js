3233c40eb1d9e276f6ca4f7bca29c88d848a9629
carternadain <cnadain@gmail.com>
2025-05-08 21:59:27 -0700

    update new quote gen project

---
+++ quoteGen/app.js
@@ -0,0 +1,7 @@
+let newQuote = document.getElementById("new-quote")
+
+let quotes = ["better in the sun","always a new leaf on the other side","lifes full of ups and downs"]
+
+newQuote.addEventListener("click" ,function(){
+    console.log("it worked")
+})
