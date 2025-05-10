356ca7f2a2468464a65f49e0d05be07f5c6f9104
carternadain <cnadain@gmail.com>
2025-05-09 19:08:52 -0700

    update app

--- quoteGen/app.js
+++ quoteGen/app.js
@@ -1,7 +1,9 @@
 let newQuote = document.getElementById("new-quote")
-
+let quoteDisplay = document.getElementById("quote")
 let quotes = ["better in the sun","always a new leaf on the other side","lifes full of ups and downs"]
 
 newQuote.addEventListener("click" ,function(){
+    let randomQuote = Math.floor(Math.random() * quotes.length)
+    quoteDisplay.textContent = "quote of the day: " + quotes[randomQuote]
     console.log("it worked")
 })
