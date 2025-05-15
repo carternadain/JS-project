bf3ae767403e00ef6cb55d355cf24990dacd8179
carternadain <cnadain@gmail.com>
2025-05-14 23:48:09 -0700

    update project

---
+++ stopLight/app.js
@@ -0,0 +1,14 @@
+let redLight = document.getElementById("red")
+let greenLight = document.getElementById("green")
+let yellowLight = document.getElementById("yellow")
+
+
+let next = document.getElementById("next")
+
+let lights = ["redlight", "greenlight", "yellowLight"]
+let currentIndex = 0
+
+next.addEventListener("click", function(){
+    currentIndex.classList.remove("on")
+   
+})
