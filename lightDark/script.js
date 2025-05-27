f3db1bedb14915681861485f70846a59c9deeb8e
carternadain <cnadain@gmail.com>
2025-05-26 22:49:47 -0700

    update test

--- lightDark/script.js
+++ lightDark/script.js
@@ -8,3 +8,7 @@ toggleBtn.addEventListener("click", function(){
 })
 
 let toggleBtn = document.getElementById("toggle-btn")
+
+toggleBtn.addEventListener("click", function(){
+    console.log(toggleBtn)
+})
